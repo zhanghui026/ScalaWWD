e096770c19b39893a5990ad39160b867682fe3af
unknown <zhanghui@.(none)>
2011-08-01 16:04:32 +0800

    //提交

---
+++ src/main/scala/coeus/nnd/geom/HelloGit.scala
@@ -0,0 +1,13 @@
+package coeus.nnd.geom
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: zhanghui
+ * Date: 11-8-1
+ * Time: 下午4:03
+ * To change this template use File | Settings | File Templates.
+ */
+
+class HelloGit {
+
+}
